86309c24bad59ce4e4fb42b9f2e16ff7d476b2a7
Matthew <pugmatt@gmail.com>
2019-06-02 23:56:21 -0400

    Start of servers route

--- website/app.js
+++ website/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var serversRouter = require('./routes/servers');
 
 var database = require('./database');
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/servers', serversRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ website/routes/servers.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var models = require('./models')
+
+/* GET users listing. */
+router.get('/servers', function(req, res, next) {
+  
+});
+
+module.exports = router;
