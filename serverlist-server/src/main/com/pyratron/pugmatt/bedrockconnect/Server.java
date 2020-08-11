9b468c1d5ec1d5273c9485c05e58fc0bb5da7146
Pugmatt <pugmatt@gmail.com>
2020-08-11 13:38:28 -0400

    Update to 1.16.20

--- serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/Server.java
+++ serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/Server.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.nukkitx.protocol.bedrock.*;
-import com.nukkitx.protocol.bedrock.v407.Bedrock_v407;
+import com.nukkitx.protocol.bedrock.v408.Bedrock_v408;
 import main.com.pyratron.pugmatt.bedrockconnect.listeners.PacketHandler;
 import main.com.pyratron.pugmatt.bedrockconnect.sql.Data;
 
@@ -74,7 +74,7 @@ public class Server {
         players = new ArrayList<>();
 
         InetSocketAddress bindAddress = new InetSocketAddress("0.0.0.0", Integer.parseInt(port));
-        codec = Bedrock_v407.V407_CODEC;
+        codec = Bedrock_v408.V408_CODEC;
         server = new BedrockServer(bindAddress);
         pong = new BedrockPong();
         pong.setEdition("MCPE");
