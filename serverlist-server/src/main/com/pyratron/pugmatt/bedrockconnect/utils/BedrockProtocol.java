aa8c7dc30699d8437f27c8ae22d93497435172f1
Camotoy <20743703+Camotoy@users.noreply.github.com>
2021-06-01 21:47:51 -0400

    1.17 support
    Supports 1.17 and doesn't drop compatibility for any older version

--- serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/utils/BedrockProtocol.java
+++ serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/utils/BedrockProtocol.java
@@ -5,6 +5,7 @@ import com.nukkitx.protocol.bedrock.v419.Bedrock_v419;
 import com.nukkitx.protocol.bedrock.v422.Bedrock_v422;
 import com.nukkitx.protocol.bedrock.v428.Bedrock_v428;
 import com.nukkitx.protocol.bedrock.v431.Bedrock_v431;
+import com.nukkitx.protocol.bedrock.v440.Bedrock_v440;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,7 @@ public class BedrockProtocol {
     /**
      * Latest available version
      */
-    public static final BedrockPacketCodec DEFAULT_BEDROCK_CODEC = Bedrock_v428.V428_CODEC;
+    public static final BedrockPacketCodec DEFAULT_BEDROCK_CODEC = Bedrock_v431.V431_CODEC;
 
     /**
      * A list of all supported Bedrock versions that can join BedrockConnect
@@ -29,8 +30,9 @@ public class BedrockProtocol {
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v422.V422_CODEC.toBuilder()
                 .minecraftVersion("1.16.200/1.16.201")
                 .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v428.V428_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(DEFAULT_BEDROCK_CODEC);
-        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v431.V431_CODEC);
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v440.V440_CODEC);
     }
 
     /**
