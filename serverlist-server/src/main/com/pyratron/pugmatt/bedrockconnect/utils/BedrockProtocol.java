73805864ced2c703b846084ae1acbf5d59cda041
Camotoy <20743703+Camotoy@users.noreply.github.com>
2022-06-02 16:45:01 -0400

    Implement 1.19 support

--- serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/utils/BedrockProtocol.java
+++ serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/utils/BedrockProtocol.java
@@ -12,6 +12,7 @@ import com.nukkitx.protocol.bedrock.v471.Bedrock_v471;
 import com.nukkitx.protocol.bedrock.v475.Bedrock_v475;
 import com.nukkitx.protocol.bedrock.v486.Bedrock_v486;
 import com.nukkitx.protocol.bedrock.v503.Bedrock_v503;
+import com.nukkitx.protocol.bedrock.v527.Bedrock_v527;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,7 @@ public class BedrockProtocol {
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v475.V475_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v486.V486_CODEC);
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v503.V503_CODEC);
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v527.V527_CODEC);
     }
 
     /**
