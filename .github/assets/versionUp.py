cefe958e9dd8b50be719587c626e0b12f4ca694e
Pugmatt <pugmatt@gmail.com>
2025-06-17 14:41:14 -0400

    Set versionUp to read from 'release' variable

--- .github/assets/versionUp.py
+++ .github/assets/versionUp.py
@@ -1,20 +1,20 @@
-# modules
-import urllib.request as request
-import json
+import re
+import sys
+from pathlib import Path
 
-# fetch newest version
-req = request.Request("https://api.github.com/repos/Pugmatt/BedrockConnect/releases/latest")
-req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0") # avoid 403 erros
-req = json.loads(request.urlopen(req).read().decode("utf-8"))
+# Read 'release' variable from BedrockConnect.java to use for new version
 
-# bump version +1
-ver_string = req["tag_name"].split(".")
-if ver_string[1] == "99":
-    string_zero = int(ver_string[0])+1
-    ver_string = str(string_zero)+"."+"0"
+file_path = Path("serverlist-server/src/main/com/pyratron/pugmatt/bedrockconnect/BedrockConnect.java")
+
+try:
+    content = file_path.read_text()
+except FileNotFoundError:
+    sys.stderr.write(f"File not found: {file_path}\n")
+    sys.exit(1)
+
+ver_string = re.search(r'String\s+release\s*=\s*"([^"]+)"', content)
+if ver_string:
+    print(ver_string.group(1))
 else:
-    string_one = int(ver_string[1])+1
-    ver_string = ver_string[0]+"."+str(string_one)
-
-# print new ver
-print(ver_string)
+    sys.stderr.write("No release version found in file.\n")
+    sys.exit(1)
